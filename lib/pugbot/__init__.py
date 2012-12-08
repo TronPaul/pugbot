606af8d254803eb233837e5e120d7b87d0d3bb95
Mark McGuire <mark.b.mcg@gmail.com>
2012-12-08 18:24:36 +0000

    first commit

---
+++ lib/pugbot/__init__.py
@@ -0,0 +1,1 @@
+from pugbot import PugBot
---
+++ lib/pugbot/pugbot.py
@@ -0,0 +1,5 @@
+from geventirc import Bot
+
+class PugBot(Bot):
+    def __init__(self, *args, **kwargs):
+        super(self, PugBot).__init__(self, *args, **kwargs)
